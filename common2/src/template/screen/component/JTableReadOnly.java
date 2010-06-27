c77f4ed8de4462c86785bbfbc268d1520e7de481
sales.softlabs <sales.softlabs@gmail.com>
2010-06-27 00:35:34 +0000

    ESS request for change of grading.

--- common2/src/template/screen/component/JTableReadOnly.java
+++ common2/src/template/screen/component/JTableReadOnly.java
@@ -4,8 +4,6 @@
  */
 package template.screen.component;
 
-import common2.Common2View;
-import java.awt.Color;
 import java.awt.Component;
 import java.awt.GridLayout;
 import java.awt.Insets;
@@ -16,13 +14,9 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.RandomAccessFile;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,7 +28,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ListCellRenderer;
 import javax.swing.ListModel;
@@ -47,11 +40,13 @@ import javax.swing.table.TableColumn;
 import org.jdesktop.beansbinding.ELProperty;
 import org.jdesktop.jdic.desktop.Desktop;
 
-
 import rule.BusinessRuleWrapper;
-import service.util.AbstractIBean;
 import template.report.AbstractReportTemplate;
-import util.*;
+import util.BeanUtil;
+import util.DataUtil;
+import util.PanelUtil;
+
+import common2.Common2View;
 
 /**
  *
