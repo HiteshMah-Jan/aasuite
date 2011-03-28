fde961f4da549be2d08e83137edd511f2e3efefd
sales.softlabs <sales.softlabs@gmail.com>
2011-03-28 01:06:59 +0000

    Update Sacred Heart Reports.

--- tmp/designing/clients/SHJ/formula/gpaformulaG1.js
+++ tmp/designing/clients/SHJ/formula/gpaformulaG1.js
@@ -1,4 +1,4 @@
 function runFormula(e) {
-	total = parseInt(e.qallCCF+.5) + parseInt(e.qallFilipino+.5) + parseInt(e.qallScience+.5) + parseInt(e.qallEnglish+.5) + parseInt(e.qallMath+.5) + parseInt(e.qallMakabayan+.5);
-	return total/6;
+	total = parseInt(e.qallCCF+.5) + parseInt(e.qallFilipino+.5) + parseInt(e.qallEnglish+.5) + parseInt(e.qallMath+.5) + parseInt(e.qallMakabayan+.5);
+	return total/5;
 }
